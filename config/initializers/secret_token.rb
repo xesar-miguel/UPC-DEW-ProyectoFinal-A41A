b9677ef4da5f01da2c738f21e9a54c702f9d78b6
unknown <usd32840@PECLIAMIR2NB008.code1.emi.philips.com>
2013-05-16 16:59:36 -0500

    Subida de codigo inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ising::Application.config.secret_token = '566dcd598efb8dcba4b7c113360fca0c5dfecdf6a500029a66d58c86647c2ecc6a023f91d0d58b799ef107b67fac74d1343b3a587fdca48f61cf1ff121897803'
